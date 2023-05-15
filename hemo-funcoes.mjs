405b69d4531d83dc22b5c93b9a3e06510c491115
Marcelo <sl_marcelo@hotmail.com>
2023-05-14 21:18:15 -0300

    Ajustes para colocar letras em "Caixa Alta"

--- hemo-funcoes.mjs
+++ hemo-funcoes.mjs
@@ -5,11 +5,12 @@ export function cadastrarDoador(nome, idade, tipoSanguineo) {
         nome = question('Nome do doador: ')
         idade = question('Idade do doador: ')
         tipoSanguineo = question('Tipo Sanguineo: ')
-        console.log(`O nome do doador é: ${nome}.`)
+        console.log('------------------------- DETALHES DO CADASTRO -------------------------')
+        console.log(`O nome do doador é: ${nome}.`.toUpperCase())
         console.log('=============================================================')
-        console.log(`A idade de ${nome} é de ${idade} anos, nascido em ${2023 - idade}.`)
+        console.log(`A idade de ${nome} é de ${idade} anos, nascido em ${2023 - idade}.`.toUpperCase())
         console.log('=============================================================')        
-        console.log(`O tipo sanguineo do doador é: ${tipoSanguineo}`)
+        console.log(`O tipo sanguineo do doador é: ${tipoSanguineo}`.toUpperCase())
         console.log('=============================================================')
 
     }
